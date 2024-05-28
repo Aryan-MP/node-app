ab1cd4a4c2ee8d17751eed3db70fee59eadadfe5
Aryan M P <aryanmangod7080@gmail.com>
2024-05-28 14:50:49 +0530

    Delete node-app directory

--- node-app/runafterclone.sh
+++
@@ -1,4 +0,0 @@
-apt install npm nodejs -y
-npm install express bcrypt winston path mongoose mongodb dotenv morgan winston-loki axios aws-sdk -y
-sudo apt-get install build-essential -y
-node server.js
